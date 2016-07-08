1943e995f935138386f22ce9a75c6774e41e2b1f
Tanya Newell <trozova5@gmail.com>
2016-07-08 13:13:47 -0400

    more logic tweaks

---
+++ main-old.js
@@ -0,0 +1,123 @@
+window.onload = function(){
+var possibleNums = [];
+  var rowLength = 4;
+  var colLength = rowLength;
+  var gameBoard = [];
+
+  function gameStart() {
+    populateGameBoard();
+    drawBoard();
+  }
+
+  function populatePossibleNums() {
+    possibleNums = [];
+    for (var i = 1; i <= rowLength; i += 1){
+      possibleNums.push(i);
+    }
+  }
+
+  function designGameBoard() {
+    for (var i = 1; i <= rowLength; i += 1){
+      gameBoard.push([]);
+    }
+  }
+
+  function randomNumIndex(){
+    var numIndex;
+    numIndex = Math.floor(Math.random() * possibleNums.length);
+    return numIndex;
+  }
+
+  function populateGameBoard() {
+    gameBoard = [];
+    designGameBoard();
+    for (var i = 0; i < colLength; i += 1) {
+      //if the row does not get set correctly, try setting it again:
+      var rowSet = false;
+      rowSet = populateGameRow(i);
+      var counter = 0;
+      while (!rowSet) {
+        if (counter > 100) {
+          console.log('counter: ', counter);
+          break;
+        }
+        console.log('resetting row i:', i);
+        console.log('gameBoard[0]: ', gameBoard[0]);
+        console.log('gameBoard[1]: ', gameBoard[1]);
+        console.log('gameBoard[2]: ', gameBoard[2]);
+        console.log('gameBoard[3]: ', gameBoard[3]);
+        rowSet = populateGameRow(i);
+        counter += 1;
+      }
+    }
+    console.log('gameBoard: ', gameBoard);
+  }
+
+  function populateGameRow(rowNum) {
+    populatePossibleNums();
+    var numToPlace, numToPlaceIndex, placed = false, placementAttempts = 0, rowSet = false;
+    for (var i = 0; i < rowLength; i += 1){
+      numToPlaceIndex = randomNumIndex();
+      numToPlace = possibleNums[numToPlaceIndex];
+
+      while (!placed) {
+        placementAttempts += 1;
+        if (validPlacement(numToPlace, rowNum, i)) {
+          gameBoard[rowNum][i] = numToPlace;
+          //possibleNums.splice(numToPlaceIndex, 1);
+          placed = true;
+        }
+        else {
+          if (placementAttempts > 15) {
+            return rowSet;
+          } else {
+            possibleNums.splice(numToPlaceIndex, 1);
+            numToPlaceIndex = randomNumIndex();
+            numToPlace = possibleNums[numToPlaceIndex];
+          }
+        }
+      }
+      populatePossibleNums();
+      placed = false;
+    }
+    if (gameBoard[rowNum][colLength - 1]) {
+      rowSet = true;
+    }
+    return rowSet;
+  }
+
+  function validPlacement(num, rowIndex, colIndex) {
+    var valid = true;
+    for (var i = 0; i < rowLength; i += 1) {
+      if (gameBoard[rowIndex][i] === num || gameBoard[i][colIndex] === num) {
+        valid = false;
+      }
+    }
+    return valid;
+  }
+
+  function drawBoard(){
+    for (var i = 0; i < rowLength; i += 1){
+      drawRow(i, gameBoard);
+    }
+  }
+
+  function drawRow(rowNum, gameBoard) {
+    //var row = document.querySelector('.row' + rowNum);
+    var boardContainer = document.querySelector('.board-flex-container');
+    for (var i = 0; i < rowLength; i += 1) {
+      var row = document.createElement('div');
+      row.classList.add('row' + i);
+      boardContainer.appendChild(row);
+    }
+
+    for (var j = 0; j < colLength; j += 1) {
+      var tile = document.createElement('span');
+      tile.innerHTML = gameBoard[rowNum][j];
+      tile.classList.add('tile');
+      row.appendChild(tile);
+    }
+  }
+
+  gameStart();
+}
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 window.onload = function(){
-var possibleNums = [];
-  var rowLength = 3;
-  var colLength = rowLength;
+  var possibleNums = [];
+  var boardLength = 4;
   var gameBoard = [];
 
   function gameStart() {
@@ -9,15 +8,18 @@ var possibleNums = [];
     drawBoard();
   }
 
-  function populatePossibleNums() {
-    possibleNums = [];
-    for (var i = 1; i <= rowLength; i += 1){
-      possibleNums.push(i);
+  function validPlacement(num, rowIndex, colIndex) {
+    var valid = true;
+    for (var i = 0; i < boardLength; i += 1) {
+      if (gameBoard[rowIndex][i] === num || gameBoard[i][colIndex] === num) {
+        valid = false;
+      }
     }
+    return valid;
   }
 
   function designGameBoard() {
-    for (var i = 1; i <= rowLength; i += 1){
+    for (var i = 0; i <= boardLength; i += 1){
       gameBoard.push([]);
     }
   }
@@ -31,30 +33,70 @@ var possibleNums = [];
   function populateGameBoard() {
     gameBoard = [];
     designGameBoard();
-    for (var i = 0; i < colLength; i += 1) {
-      populatePossibleNums();
+    for (var i = 0; i < boardLength; i += 1) {
       //if the row does not get set correctly, try setting it again:
       var rowSet = false;
       rowSet = populateGameRow(i);
+      var counter = 0;
       while (!rowSet) {
-        populatePossibleNums();
+        if (counter > 100) {
+          // console.log('counter: ', counter);
+          break;
+        }
+        //console.log('resetting row i:', i);
+        // console.log('gameBoard[0]: ', gameBoard[0]);
+        // console.log('gameBoard[1]: ', gameBoard[1]);
+        // console.log('gameBoard[2]: ', gameBoard[2]);
+        // console.log('gameBoard[3]: ', gameBoard[3]);
         rowSet = populateGameRow(i);
+        counter += 1;
       }
     }
-    console.log('gameBoard: ', gameBoard);
+    //console.log('gameBoard: ', gameBoard);
   }
 
-  function populateGameRow(rowNum) {
+  // //populate numbers that are not already contained in the column:
+  function populatePossibleNums(rowIndex, colIndex) {
+    possibleNums = [];
+    for (var i = 1; i <= boardLength; i += 1){
+      possibleNums.push(i);
+    }
+    //go through every number in column and if it contains in possibleNums, delete it:
+    var deleteNumIndex;
+    var deletedNum;
+    for (var j = 0; j <= boardLength; j += 1){
+      if (possibleNums.includes(gameBoard[j][colIndex])) {
+        deleteNumIndex = possibleNums.indexOf(gameBoard[j][colIndex]);
+        deletedNum = possibleNums.splice(deleteNumIndex, 1);
+        console.log('deletedNum: ', deletedNum);
+      }
+      if (possibleNums.includes(gameBoard[rowIndex][j])) {
+        deleteNumIndex = possibleNums.indexOf(gameBoard[rowIndex][j]);
+        deletedNum = possibleNums.splice(deleteNumIndex, 1);
+        console.log('deletedNum: ', deletedNum);
+      }
+    }
+    console.log('possibleNums: ', possibleNums);
+  }
+
+  function populateGameRow(rowIndex) {
+    // populatePossibleNums(rowIndex);
     var numToPlace, numToPlaceIndex, placed = false, placementAttempts = 0, rowSet = false;
-    for (var i = 0; i < rowLength; i += 1){
+    for (var colIndex = 0; colIndex < boardLength; colIndex += 1){
+      populatePossibleNums(rowIndex, colIndex);
+      //console.log('gameBoard: ', gameBoard);
+      console.log('setting row: ', rowIndex);
+      console.log('setting col: ', colIndex);
+      //console.log('setting tile: ', gameBoard[rowIndex][colIndex]);
+      //console.log('possibleNums: ', possibleNums);
       numToPlaceIndex = randomNumIndex();
       numToPlace = possibleNums[numToPlaceIndex];
 
       while (!placed) {
         placementAttempts += 1;
-        if (validPlacement(numToPlace, rowNum, i)) {
-          gameBoard[rowNum][i] = numToPlace;
-          possibleNums.splice(numToPlaceIndex, 1);
+        if (validPlacement(numToPlace, rowIndex, colIndex)) {
+          gameBoard[rowIndex][colIndex] = numToPlace;
+          //possibleNums.splice(numToPlaceIndex, 1);
           placed = true;
         }
         else {
@@ -67,27 +109,17 @@ var possibleNums = [];
           }
         }
       }
-      populatePossibleNums();
+      populatePossibleNums(rowIndex, colIndex);
       placed = false;
     }
-    if (gameBoard[rowNum][colLength - 1]) {
+    if (gameBoard[rowIndex][boardLength - 1]) {
       rowSet = true;
     }
     return rowSet;
   }
 
-  function validPlacement(num, rowIndex, colIndex) {
-    var valid = true;
-    for (var i = 0; i < rowLength; i += 1) {
-      if (gameBoard[rowIndex][i] === num || gameBoard[i][colIndex] === num) {
-        valid = false;
-      }
-    }
-    return valid;
-  }
-
   function drawBoard(){
-    for (var i = 0; i < rowLength; i += 1){
+    for (var i = 0; i < boardLength; i += 1){
       drawRow(i, gameBoard);
     }
   }
@@ -95,13 +127,13 @@ var possibleNums = [];
   function drawRow(rowNum, gameBoard) {
     //var row = document.querySelector('.row' + rowNum);
     var boardContainer = document.querySelector('.board-flex-container');
-    for (var i = 0; i < rowLength; i += 1) {
+    for (var i = 0; i < boardLength; i += 1) {
       var row = document.createElement('div');
       row.classList.add('row' + i);
       boardContainer.appendChild(row);
     }
 
-    for (var j = 0; j < colLength; j += 1) {
+    for (var j = 0; j < boardLength; j += 1) {
       var tile = document.createElement('span');
       tile.innerHTML = gameBoard[rowNum][j];
       tile.classList.add('tile');
